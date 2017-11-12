3707c7887e6982d863eccd1ca3828984a2209248
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2017-11-12 22:44:53 +0000

    Fix typo error

--- init.sh
+++ init.sh
@@ -12,8 +12,8 @@ while read studentConfig
 do
 	studentId=`echo $studentConfig | cut -d, -f1`
 	studentPassword=`head /dev/random | md5sum | grep -o '[a-f0-9]*' --color=never`
-	echo docker network create $studentId
-	echo docker run -d -p $studentPort:80 --net=$studentId -e VNC_PASSWORD=$studentPassword --name $studentId dorowu/ubuntu-desktop-lxde-vnc
+	docker network create $studentId
+	docker run -d -p $studentPort:80 --net=$studentId -e VNC_PASSWORD=$studentPassword --name $studentId dorowu/ubuntu-desktop-lxde-vnc
 	echo "$studentId => $studentPort : $studentPassword"
 	((studentPort++))
 	sleep 5

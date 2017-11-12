883d6ad87c4ba147b95256c71376abf9363faa5f
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2017-11-12 22:44:15 +0000

    Add password by student and refactor to accept CSV

--- init.sh
+++ init.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-count=6080
-for i in $(cat listaPcOrAlumnos.txt)
-do
-	docker network create $i #Crea una nueva red en docker 
-	docker run -d -p $count:80 --net=$i --name $i dorowu/ubuntu-desktop-lxde-vnc #Parametro --net "Agrega el contenedor a la red existente"
-	((count++))
-	sleep 5
-done
 
+if [[ "$1" == '' ]]
+then
+	echo "Usage: $0 configFile.csv"
+	exit 1
+fi
+
+studentPort=6080
+
+while read studentConfig
+do
+	studentId=`echo $studentConfig | cut -d, -f1`
+	studentPassword=`head /dev/random | md5sum | grep -o '[a-f0-9]*' --color=never`
+	echo docker network create $studentId
+	echo docker run -d -p $studentPort:80 --net=$studentId -e VNC_PASSWORD=$studentPassword --name $studentId dorowu/ubuntu-desktop-lxde-vnc
+	echo "$studentId => $studentPort : $studentPassword"
+	((studentPort++))
+	sleep 5
+done < $1

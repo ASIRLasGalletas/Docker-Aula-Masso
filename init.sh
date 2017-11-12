09612022d92de0fff5f4ed273603f2ed74d0c92c
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2017-11-12 22:52:55 +0000

    Update the image in the start script

--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ then
 	exit 1
 fi
 
+image='dorowu/ubuntu-desktop-lxde-vnc'
+
+docker pull $image
+
 studentPort=6080
 
 while read studentConfig
@@ -13,7 +17,7 @@ do
 	studentId=`echo $studentConfig | cut -d, -f1`
 	studentPassword=`head /dev/random | md5sum | grep -o '[a-f0-9]*' --color=never`
 	docker network create $studentId
-	docker run -d -p $studentPort:80 --net=$studentId -e VNC_PASSWORD=$studentPassword --name $studentId dorowu/ubuntu-desktop-lxde-vnc
+	docker run -d -p $studentPort:80 --net=$studentId -e VNC_PASSWORD=$studentPassword --name $studentId $image
 	echo "$studentId => $studentPort : $studentPassword"
 	((studentPort++))
 	sleep 5

34ab98f2e03a61b227b09972f75380bb5dec8843
Jesús Hernández Gormaz <0.jesus.hg@gmail.com>
2017-11-13 00:18:57 +0000

    Random port for each student

--- init.sh
+++ init.sh
@@ -10,15 +10,16 @@ image='dorowu/ubuntu-desktop-lxde-vnc'
 
 docker pull $image
 
-studentPort=6080
+counter=0
 
 while read studentConfig
 do
 	studentId=`echo $studentConfig | cut -d, -f1`
 	studentPassword=`head /dev/random | md5sum | grep -o '[a-f0-9]*' --color=never`
+	studentPort=3$counter`echo $RANDOM | head -c 2 && echo`
 	docker network create $studentId
 	docker run -d -p $studentPort:80 --net=$studentId -e VNC_PASSWORD=$studentPassword --name $studentId $image
 	echo "$studentId => $studentPort : $studentPassword"
-	((studentPort++))
+	((counter++))
 	sleep 5
 done < $1
